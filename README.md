34c34f4df3c8a020aff2f0df9988574e5b78e1e9
Adam Payne <ajpayne238@gmail.com>
2019-10-18 13:42:52 -0400

    Deleting

--- README.md
+++ README.md
@@ -29,8 +29,11 @@ In this project you will create a login page and request a token from the server
 Demonstrate your understanding of this Sprint's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager.
 
 - [ ] Explain what a token is used for.
+Tokens are used to get access from more secure apis that require login authentication.
 - [ ] What steps can you take in your web apps to keep your data secure?
+Private routes can be used to get data secure. Routes for admins and routes for members as well as routes for the public that have not yet signed up for services can seperate what data is accessible.
 - [ ] Describe how web servers work.
+Web servers hold information to be retrieved.
 - [ ] Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
 
 
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+// import axios from "axios";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+const ColorList  = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,10 +21,29 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colors.id}`)
+    .then(response => {
+      console.log("Put List Response", response)
+    })
+    .catch(error => {
+      console.log("", error)
+    })
+    
+    
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+   
+      axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`) 
+      .then(response => {
+        console.log("Delete Response", response)
+        updateColors(colors)
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
   };
 
   return (
